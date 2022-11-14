95e085aff1d72465230b112bda965ffcc6f5f41c
Orion Silva Simonetti <50591525+AnarkiSimonetti@users.noreply.github.com>
2022-11-14 09:08:05 -0300

    primeiros comandos

---
+++ primeiroscomandos.py
@@ -0,0 +1,44 @@
+print ("Teste");
+
+# abaixo exemplo quanto esta não tem aspas é numerico
+
+print (27);
+
+# abaixo exemplo quando esta em "" é String
+ 
+print("27");
+
+"""
+    # nota-se que se eu tentar executar o comando sem " " em um texto ele vai apresentar um erro porque ele esta sem as aspas
+    # identificando que isso é um texto/string
+ 
+"""
+
+print("Teste");
+
+
+""" Concatenando exemplos
+
+    print("Esse é teste" 4); ao tentar usar o comando assim ele vai dar erro "erro de sintaxe" eu preciso 
+    juntar/unir  o texto com o numero, variavel ou outra string para isso basta a gente adicionar a ,
+
+    veja exemplo de como fazer o comando
+    
+    """
+
+print("esse é o teste", 4);
+
+# agora um exemplo caso eu queira concatenar texto, numero e texto novamente
+
+
+print("Eu tenho", 33, ("anos"))
+
+# agora exemplos de como imprimir em duas linhas eu posso fazer de duas formas
+
+print("Ola")
+print("teste")
+
+
+# ou posso usar o \n (O \n serve para dar uma quebra de linha)
+
+print("Teste \n Matrix")
